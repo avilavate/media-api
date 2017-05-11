69b99c17224be65c2ba030c2caaa52d4df054547
avilavate <avilavate123@gmail.com>
2017-05-11 16:16:52 +0530

    ifixing oc error

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('port',  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,);
+app.set('port',  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
 app.set('ip',  process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
