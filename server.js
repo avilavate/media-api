ee8d90aa49d7fc593de1ebfa19b7b18d2167365e
avilavate <avilavate123@gmail.com>
2017-05-11 12:12:48 +0530

    openshift port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 58902);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
