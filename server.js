248996d266b950ff40f04d191b41ae616757abd4
avilavate <avilavate123@gmail.com>
2017-05-11 16:27:59 +0530

    adding content category in res.

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('port',  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip',  process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
-http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+    res.setHeader('content-type', 'text/javascript');
+    res.send('Hello World!');
 });
