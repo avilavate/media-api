4597a1577a81069d33e8773e329eca0e71246eee
avilavate <avilavate123@gmail.com>
2017-05-11 16:07:08 +0530

    debugging openshift hosting issues.

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var http = require('http');
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.OPENSHIFT_NODEJS_IP || 'localhost');
+app.set('port',  process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,);
+app.set('ip',  process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){
   console.log('Express server listening on port ' + app.get('port'));
