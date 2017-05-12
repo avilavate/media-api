6c4bb5be80696b00202eeffcec903fe3a9ebad8a
avilavate <avilavate123@gmail.com>
2017-05-12 17:07:17 +0530

    QS params (needed to send artist id (POC))

--- server.js
+++ server.js
@@ -3,14 +3,15 @@ var https = require('https');
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
-app.set('ip', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 58080);
+app.set('ip', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || 'localhost');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
 
 app.get('/', function (req, res) {
+
     res.setHeader('content-type', 'text/javascript');
     res.send('Hello World!');
 });
@@ -33,3 +34,20 @@ app.get('/tracks', function (req, res) {
         });
 
 });
+
+app.get("/test", function (req, res) {
+    console.dir(req.query);
+    var body = '';
+  return  https.get("https://jsonplaceholder.typicode.com/posts/1", function (response) {
+
+        response.on('data', function (d) {
+            body += d;
+        });
+
+        response.on('end', function () {
+
+            // Data reception is done, do whatever with it!
+           return res.send(200,body);
+        });
+    });
+   });
