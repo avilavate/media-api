da2cb52b5c12f1e5918134407c411c9f9f0ea05b
avilavate <avilavate123@gmail.com>
2017-05-15 13:03:10 +0530

    added validations with sample app and tracks api endpont

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ var https = require('https');
 var express = require('express');
 var app = express();
 
-app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 58080);
-app.set('ip', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || 'localhost');
+app.set('port', process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('ip', process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function () {
     console.log('Express server listening on port ' + app.get('port'));
@@ -38,16 +38,19 @@ app.get('/tracks', function (req, res) {
 app.get("/test", function (req, res) {
     console.dir(req.query);
     var body = '';
-  return  https.get("https://jsonplaceholder.typicode.com/posts/1", function (response) {
+    if (req.query.post === undefined || parseInt(req.query.post) !== NaN) res.send(400, 'invalid query');
+    else {
+        return https.get(`https://jsonplaceholder.typicode.com/posts/${req.query.post}`, function (response) {
 
-        response.on('data', function (d) {
-            body += d;
+            response.on('data', function (d) {
+                body += d;
+            });
+
+            response.on('end', function () {
+
+                // Data reception is done, do whatever with it!
+                return res.send(200, body);
+            });
         });
-
-        response.on('end', function () {
-
-            // Data reception is done, do whatever with it!
-           return res.send(200,body);
-        });
-    });
-   });
+    }
+});
